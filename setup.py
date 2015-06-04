c50c51f545743adcc6a49553c41a7d06fbc11a7f
Tamarabyte <TamaraByte@gmail.com>
2015-06-03 20:13:26 -0600

    Modified setup.

--- setup.py
+++ setup.py
@@ -18,4 +18,14 @@ setup(
     py_modules=['timeUUID'],
     platforms='any',
     test_suite='test_timeUUID',
+    
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    ]
 )
